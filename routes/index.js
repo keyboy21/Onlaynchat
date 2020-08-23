6a0651046fd24d7e3f24e1d6683fac758c0409ab
yusuf <yusufsaydullayev@gmail.com>
2020-08-23 18:53:30 +0500

    Fixed

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Live ball chat' });
-// });
+// /* GET home page. 
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Live ball chat' });
+});
 
 
-router.get('/getInv', function(req, res, next) {
+router.get('/getInv', function (req, res, next) {
   const envData = require('../cfg/config.json')[process.env.NODE_ENV || 'development']
   res.json(envData)
 });
